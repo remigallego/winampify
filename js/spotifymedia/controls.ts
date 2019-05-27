27427bd18d0ea4d04da4880cd65bb5c5351eb0ff
remi.gallego@gmail.com <remi.gallego@gmail.com>
2019-05-27 22:19:11 +0200

    refactor(spotifymedia/controls): cosmetic

--- js/spotifymedia/controls.ts
+++ js/spotifymedia/controls.ts
@@ -1,10 +1,8 @@
 import dragMock from "drag-mock";
 
 export default class WebampControls {
-  constructor() {}
-
-  /* 
-    Workaround: In order to remove all tracks from the playlist and play a new track, 
+  /*
+    Workaround: In order to remove all tracks from the playlist and play a new track,
      we need to mock a drag drop event in the main window
   */
   static clearPlaylistAndLoadFile(fileId: string) {
