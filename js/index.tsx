1ef97238166f0b43aa623cdaaaf822ccdaa43383
remi.gallego@gmail.com <remi.gallego@gmail.com>
2019-05-28 23:22:53 +0200

    feat: add sentry

--- js/index.tsx
+++ js/index.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/browser";
 import "babel-polyfill";
 import React from "react";
 import { render } from "react-dom";
@@ -7,6 +8,10 @@ import { PersistGate } from "redux-persist/integration/react";
 import "../css/line-scale.css";
 import Winampify from "./components";
 import store from "./store";
+
+Sentry.init({
+  dsn: "https://6ee628e2853b493ca3872c3b9daf766d@sentry.io/1469964"
+});
 
 // tslint:disable-next-line: no-console
 console.log(`ENV = ${process.env.NODE_ENV}`);
