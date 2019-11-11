300464151eaf9ad1d1181e943353bb70f514b00a
Remi <remi.gallego@gmail.com>
2019-11-11 13:33:39 +0100

    chore: update typescript

--- js/components/Desktop/FileItem.tsx
+++ js/components/Desktop/FileItem.tsx
@@ -15,76 +15,68 @@ interface Props {
   selected: boolean;
 }
 
-class FileItem extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
+const FileItem = (props: Props) => {
+  const { file } = props;
 
-  render() {
-    const { file } = this.props;
+  const getIcon = () => {
+    switch (file.metaData.type) {
+      case "track":
+        return bigWinampIcon;
+      case "album":
+      case "action":
+      case "artist":
+        return folderclosed;
+      case "image":
+        return file.metaData.url;
+      default:
+        return bigWinampIcon;
+    }
+  };
 
-    const getIcon = () => {
-      switch (file.metaData.type) {
-        case "track":
-          return bigWinampIcon;
-        case "album":
-        case "action":
-        case "artist":
-          return folderclosed;
-        case "image":
-          return file.metaData.url;
-        default:
-          return bigWinampIcon;
-      }
-    };
-
-    return (
-      <ContextMenuProvider id={file.metaData.type}>
-        <div
-          className="file-fadein"
-          style={{
-            width: "100px",
-            position: "absolute",
-            left: file.x,
-            top: file.y,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center"
-          }}
-          id={file.id}
-          onMouseDown={this.props.onClick}
-          onDoubleClick={this.props.onDoubleClick}
-        >
-          <img src={getIcon()} style={DesktopFileStyle.image} />
-          {file.isRenaming ? (
-            <InputRenaming
-              initialValue={file.title}
-              confirmRenameFile={this.props.confirmRenameFile}
-            />
-          ) : (
-            <div
-              style={{
-                fontSize: "14px",
-                textAlign: "center",
-                color: "white",
-                textShadow: "1px 1px black",
-                backgroundColor: this.props.selected
-                  ? "#3064BD"
-                  : "transparent",
-                border: this.props.selected
-                  ? "1px dotted white"
-                  : "1px dotted transparent",
-                borderStyle: "dotted",
-                boxSizing: "border-box"
-              }}
-            >
-              {file.title}
-            </div>
-          )}
-        </div>
-      </ContextMenuProvider>
-    );
-  }
-}
+  return (
+    <ContextMenuProvider id={file.metaData.type}>
+      <div
+        className="file-fadein"
+        style={{
+          width: "100px",
+          position: "absolute",
+          left: file.x,
+          top: file.y,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+        id={file.id}
+        onMouseDown={props.onClick}
+        onDoubleClick={props.onDoubleClick}
+      >
+        <img src={getIcon()} style={DesktopFileStyle.image} />
+        {file.isRenaming ? (
+          <InputRenaming
+            initialValue={file.title}
+            confirmRenameFile={props.confirmRenameFile}
+          />
+        ) : (
+          <div
+            style={{
+              fontSize: "14px",
+              textAlign: "center",
+              color: "white",
+              textShadow: "1px 1px black",
+              backgroundColor: props.selected ? "#3064BD" : "transparent",
+              border: props.selected
+                ? "1px dotted white"
+                : "1px dotted transparent",
+              borderStyle: "dotted",
+              boxSizing: "border-box"
+            }}
+          >
+            {file.title}
+          </div>
+        )}
+      </div>
+    </ContextMenuProvider>
+  );
+};
 
 export default FileItem;
