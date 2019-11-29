d0857f33ab1d4403d98e9140b937f06a33b7e087
Remi <remi.gallego@gmail.com>
2019-11-29 14:45:13 +0100

    refactor(InputRenaming): rerfactor to hooks

--- js/components/Desktop/InputRenaming.tsx
+++ js/components/Desktop/InputRenaming.tsx
@@ -1,43 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   initialValue: string;
   confirmRenameFile: (e: any) => void;
 }
 
-interface State {
-  text: string;
-}
+const InputRenaming = (props: Props) => {
+  const [text, setText] = useState(props.initialValue);
 
-class InputRenaming extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { text: this.props.initialValue };
-  }
-  render() {
-    return (
-      <form onSubmit={this.props.confirmRenameFile}>
-        <input
-          style={{
-            fontSize: "14px",
-            textAlign: "center",
-            backgroundColor: "white",
-            textShadow: "none",
-            color: "black",
-            border: "1px dotted transparent",
-            borderStyle: "dotted",
-            boxSizing: "border-box",
-            outline: "none",
-            margin: 0,
-            padding: 0
-          }}
-          ref={`test`}
-          onChange={text => this.setState({ text: text.target.value })}
-          value={this.state.text}
-        />
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={props.confirmRenameFile}>
+      <input
+        style={{
+          fontSize: "14px",
+          textAlign: "center",
+          backgroundColor: "white",
+          textShadow: "none",
+          color: "black",
+          border: "1px dotted transparent",
+          borderStyle: "dotted",
+          boxSizing: "border-box",
+          outline: "none",
+          margin: 0,
+          padding: 0
+        }}
+        onChange={val => setText(val.target.value)}
+        value={text}
+      />
+    </form>
+  );
+};
 
 export default InputRenaming;
