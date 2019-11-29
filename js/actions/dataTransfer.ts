e88c94ccf23f9c9d420867a110e059df79a8cff7
Remi <remi.gallego@gmail.com>
2019-11-29 14:45:29 +0100

    refactor: rename data transfer function

--- js/actions/dataTransfer.ts
+++ js/actions/dataTransfer.ts
@@ -2,7 +2,7 @@ import { Action, Dispatch } from "redux";
 import { SET_DATA_TRANSFER_ARRAY } from "../reducers/dataTransfer";
 import { WebampTrackFormat } from "../types";
 
-export function setDataTransfer(data: WebampTrackFormat[]): any {
+export function setWebampDataTransfer(data: WebampTrackFormat[]): any {
   return (dispatch: Dispatch<Action>) => {
     dispatch({
       type: SET_DATA_TRANSFER_ARRAY,
--- js/actions/playback.ts
+++ js/actions/playback.ts
@@ -2,13 +2,13 @@ import { Dispatch } from "react";
 import { Action } from "redux";
 import { PLAY } from "../reducers/playback";
 import { TrackFile } from "../types";
-import { setDataTransfer } from "./dataTransfer";
+import { setWebampDataTransfer } from "./dataTransfer";
 import { formatMetaToWebampMeta } from "../utils/dataTransfer";
 import WebampControls from "../spotifymedia/controls";
 
 export function playTrack(file: TrackFile): any {
   return (dispatch: Dispatch<Action>) => {
-    dispatch(setDataTransfer([formatMetaToWebampMeta(file.metaData)]));
+    dispatch(setWebampDataTransfer([formatMetaToWebampMeta(file.metaData)]));
     setTimeout(() => WebampControls.mockWindowDrop(), 0);
     dispatch({
       type: PLAY
