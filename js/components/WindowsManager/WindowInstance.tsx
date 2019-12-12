9b575c6a5c7ac0953b9f9da73293acedec75e071
Remi <remi.gallego@gmail.com>
2019-12-12 18:58:59 +0100

    refactor(WindowInstance): refactor to Hooks

--- js/components/WindowsManager/WindowInstance.tsx
+++ js/components/WindowsManager/WindowInstance.tsx
@@ -6,32 +6,20 @@ interface Props {
   children: ReactNode;
 }
 
-interface State {
-  animation: string;
-}
-
-class WindowInstance extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div
-        className="window-instance---"
-        style={{
-          backgroundColor: "red",
-          zIndex: this.props.zIndex,
-          position: "absolute"
-        }}
-        onMouseDown={(e: any) => {
-          if (e.target.id !== "disallow-on-top") this.props.setOnTop();
-        }}
-      >
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-export default WindowInstance;
+export default (props: Props) => {
+  return (
+    <div
+      className="window-instance---"
+      style={{
+        backgroundColor: "red",
+        zIndex: props.zIndex,
+        position: "absolute"
+      }}
+      onMouseDown={(e: any) => {
+        if (e.target.id !== "disallow-on-top") props.setOnTop();
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
