2cb9aaf34a34d173f480dc9e411bf1f6ddcdb18a
Remi <remi.gallego@gmail.com>
2019-12-26 18:16:52 +0100

    fix(explorers): fix getActiveExplorer

--- js/utils/explorer.ts
+++ js/utils/explorer.ts
@@ -1,16 +1,18 @@
 import uuidv1 from "uuid/v1";
 import { AppState } from "../reducers";
 import { WINDOW_TYPE } from "../reducers/windows";
+import { findHighestPosition } from "./windows";
 
 export const generateExplorerId = () => {
   return `explorer_${uuidv1()}`;
 };
 
 export const getActiveExplorerId = (state: AppState): string => {
-  const activeExplorer = state.windows.windows
-    .slice()
-    .reverse()
-    .find(windowItem => windowItem.type === WINDOW_TYPE.Explorer);
+  const explorers = state.windows.windows.filter(
+    windowItem => windowItem.type === WINDOW_TYPE.Explorer
+  );
+  const ff = findHighestPosition(explorers);
+  const activeExplorer = explorers.find(e => e.position === ff);
 
   if (activeExplorer) return activeExplorer.id;
   else {
