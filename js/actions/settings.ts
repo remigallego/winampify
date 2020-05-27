8c5ec0bc4d1e5a330c1817415ae47301e37a51ad
Remi <remi.gallego@hybridheroes.de>
2020-05-27 17:11:18 +0800

    feat: add dark background color to dark mode

--- js/actions/settings.ts
+++ js/actions/settings.ts
@@ -15,19 +15,29 @@ export function toggleSettingsMenu() {
 export function toggleDarkMode() {
   return (dispatch: Dispatch<Action>, getState: () => AppState) => {
     if (getState().settings.isDarkMode) {
+      const htmlDoc = window.document.getElementsByTagName("html");
+      htmlDoc[0].style.background = `linear-gradient(
+        184deg,
+        rgba(97, 68, 255, 1) 0%,
+        rgba(6, 46, 120, 1) 100%
+      )`;
       dispatch({
         type: SET_THEME,
         payload: {
           theme: defaultTheme
         }
       });
-    } else
+    } else {
+      const htmlDoc = window.document.getElementsByTagName("html");
+      htmlDoc[0].style.background =
+        "linear-gradient(184deg, rgba(18,12,47,1) 0%, rgba(6,1,31,1) 100%)";
       dispatch({
         type: SET_THEME,
         payload: {
           theme: darkTheme
         }
       });
+    }
     dispatch({
       type: TOGGLE_DARK_MODE
     });
